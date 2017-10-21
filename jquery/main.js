cbf5c718003077d6faded671871541a2b8f941c3
Elvis de Freitas <edigitalb@gmail.com>
2017-10-21 15:20:33 -0200

    completing example

--- jquery/main.js
+++ jquery/main.js
@@ -4,39 +4,14 @@
 $(function () {
 
 	/**
-	 * Try to load a file with the passed URL in the 'data' folder
-	 * then put it in the #content div
-	 */
-	function load (url) {
-		console.debug('m=load, url=%s', url)
-		$.get('data/' + url).done(function (data) {
-				$("#content").html(data);
-		}).fail(function(res){
-			console.error('m=load, status=%d, err=%o', res.status, arguments);
-			wrapIframe($("#content").empty(), '<h2>Error at load page "/' + url + '"</h2>' + res.responseText);
-		})
-	}
-	
-	/**
 	 * handling all link clicks and  forwarding it to ajax calls
 	 */
 	$(document).on('click', 'a', function (e) {
-			e.preventDefault();
-
-			var $this = $(this),
-					url = $this.attr("href"),
-					title = $this.text();
-
-			history.pushState({
-					url: url,
-					title: title
-			}, title, url);
-
-			// currently browsers does not consider the pushState title property
-			// so we need to set it using document
-			document.title = title;
-
-			load(url);
+		e.preventDefault();
+		var $this = $(this),
+			url = $this.attr("href"),
+			title = $this.text();
+		doLoad(url, title)
 	});
 
 	// when a state change we must load the new page
@@ -59,10 +34,37 @@ $(function () {
 		iframedoc = ifr.contentDocument || ifr.contentWindow.document;
 		iframedoc.body.innerHTML = html
 	}
+
+	/**
+	 * Try to load a file with the passed URL in the 'data' folder
+	 * then put it in the #content div
+	 */
+	function load (url) {
+		console.debug('m=load, url=%s', url)
+		$.get('data/' + url).done(function (data) {
+				$("#content").html(data);
+		}).fail(function(res){
+			console.error('m=load, status=%d, err=%o', res.status, arguments);
+			wrapIframe($("#content").empty(), '<h2>Error at load page "/' + url + '"</h2>' + res.responseText);
+		})
+	}
+
+	/**
+	 * Change the state of the page and load the content
+	 */
+	function doLoad(url, title){
+		history.pushState({
+			url: url,
+			title: title
+		}, title, url);
+
+		// currently browsers does not consider the pushState title property
+		// so we need to set it using document
+		document.title = title;
+
+		load(url);
+	}
 	
-	history.pushState({
-		url: 'south-america',
-		title: 'South America'
-	}, 'South America', 'south-america');
+	doLoad('south-america', 'South America');
 	
 });
