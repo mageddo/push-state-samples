9efd94101a1bb711a62274149b8bb04de07adaae
Elvis de Freitas <edigitalb@gmail.com>
2017-10-21 16:45:22 -0200

    dockerizing

--- jquery/server.go
+++ jquery/server.go
@@ -7,14 +7,14 @@ import (
 
 func main(){
 
-	// mapeia uma pasta de estáticos
+	// mapping static files
 	http.Handle("/", http.FileServer(http.Dir("./")))
 
+	// all pages will load content from the root static directory. Then onpopstate will take care of 
+	// load the respective page content
 	http.HandleFunc("/page/", func(res http.ResponseWriter, req *http.Request){
 		log.Println("m=page, path=", req.URL.Path)
 		http.StripPrefix(req.URL.Path, http.FileServer(http.Dir("./"))).ServeHTTP(res, req)
 	})
 	log.Fatal(http.ListenAndServe(":3030", nil))
 }
-
-// 
